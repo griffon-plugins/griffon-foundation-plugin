773ba48c858577fadc0b8098a1c6ea15371b7491
aalmiray <aalmiray@gmail.com>
2015-08-20 17:24:40 +0200

    Enable JavaFX properties on JavaFX package

--- subprojects/griffon-foundation-javafx/src/main/java/griffon/javafx/support/foundation/FoundationIcon.java
+++ subprojects/griffon-foundation-javafx/src/main/java/griffon/javafx/support/foundation/FoundationIcon.java
@@ -17,7 +17,14 @@ package griffon.javafx.support.foundation;
 
 import griffon.core.editors.PropertyEditorResolver;
 import griffon.plugins.foundation.Foundation;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
@@ -34,7 +41,8 @@ import static java.util.Objects.requireNonNull;
  */
 public class FoundationIcon extends Text {
     private static final String FOUNDATION_SET = "META-INF/resources/foundation/3.0/fonts/foundation-icons.ttf";
-    private static final String ERROR_FONT_FOUNDATION_NULL = "Argument 'foundation' must not be null";
+    private static final String ERROR_FOUNDATION_NULL = "Argument 'foundation' must not be null";
+
     private static final String FOUNDATION_FONT_FAMILY;
 
     static {
@@ -42,19 +50,33 @@ public class FoundationIcon extends Text {
         FOUNDATION_FONT_FAMILY = font.getFamily();
     }
 
-    private Foundation foundation;
-    private int iconSize;
-    private Color iconColor;
+    private ObjectProperty<Foundation> foundation;
+    private IntegerProperty iconSize;
+    private ObjectProperty<Paint> iconColor;
+
+    private ChangeListener<Number> iconSizeChangeListener = new ChangeListener<Number>() {
+        @Override
+        public void changed(ObservableValue<? extends Number> v, Number o, Number n) {
+            setStyle(getStyle() + " -fx-font-size: " + n + "px;");
+        }
+    };
+
+    private ChangeListener<Paint> iconColorChangeListener = new ChangeListener<Paint>() {
+        @Override
+        public void changed(ObservableValue<? extends Paint> v, Paint o, Paint n) {
+            setFill(n);
+        }
+    };
 
     public FoundationIcon() {
         this(Foundation.FI_STAR);
     }
 
     public FoundationIcon(@Nonnull Foundation foundation) {
-        this.foundation = requireNonNull(foundation, ERROR_FONT_FOUNDATION_NULL);
+        setFoundation(requireNonNull(foundation, ERROR_FOUNDATION_NULL));
         getStyleClass().add("foundation-icon");
         setText(String.valueOf(foundation.getCode()));
-        setStyle("-fx-font-family: " + FOUNDATION_FONT_FAMILY + ";");
+        setStyle("-fx-font-family: '" + FOUNDATION_FONT_FAMILY + "';");
         setIconSize(16);
         setIconColor(Color.BLACK);
     }
@@ -62,45 +84,78 @@ public class FoundationIcon extends Text {
     public FoundationIcon(@Nonnull String description) {
         this(Foundation.findByDescription(description));
         resolveSize(description);
-        resolveColor(description);
+        resolvePaint(description);
+    }
+
+    public ObjectProperty<Foundation> foundationProperty() {
+        if (foundation == null) {
+            foundation = new SimpleObjectProperty<>(this, "foundation", null);
+        }
+        return foundation;
+    }
+
+    public ObjectProperty<Foundation> getFoundationProperty() {
+        return foundationProperty();
+    }
+
+    public IntegerProperty iconSizeProperty() {
+        if (iconSize == null) {
+            iconSize = new SimpleIntegerProperty(this, "iconSize", 16);
+            iconSize.addListener(iconSizeChangeListener);
+        }
+        return iconSize;
+    }
+
+    public IntegerProperty getIconSizeProperty() {
+        return iconSizeProperty();
+    }
+
+    public ObjectProperty<Paint> iconColorProperty() {
+        if (iconColor == null) {
+            iconColor = new SimpleObjectProperty<>(this, "iconColor", null);
+            iconColor.addListener(iconColorChangeListener);
+        }
+        return iconColor;
+    }
+
+    public ObjectProperty<Paint> getIconColorProperty() {
+        return iconColorProperty();
     }
 
     @Nonnull
     public Foundation getFoundation() {
-        return foundation;
+        return foundationProperty().get();
     }
 
     public void setFoundation(@Nonnull Foundation foundation) {
-        this.foundation = requireNonNull(foundation, ERROR_FONT_FOUNDATION_NULL);
+        foundationProperty().set(requireNonNull(foundation, ERROR_FOUNDATION_NULL));
         setText(String.valueOf(foundation.getCode()));
     }
 
     public void setFoundation(@Nonnull String description) {
         requireNonBlank(description, "Argument 'description' must not be blank");
-        setFoundation(Foundation.findByDescription(description));
+        foundationProperty().set(Foundation.findByDescription(description));
         resolveSize(description);
-        resolveColor(description);
+        resolvePaint(description);
     }
 
     public void setIconSize(int size) {
-        requireState(size > 0, "Argument 'size' must be greater than zero");
-        setStyle(getStyle() + " -fx-font-size: " + size + "px;");
-        this.iconSize = size;
+        requireState(size > 0, "Argument 'size' must be greater than zero.");
+        iconSizeProperty().set(size);
     }
 
     public int getIconSize() {
-        return iconSize;
+        return iconSizeProperty().get();
     }
 
-    public void setIconColor(@Nonnull Color color) {
+    public void setIconColor(@Nonnull Paint color) {
         requireNonNull(color, "Argument 'color' must not be null");
-        setFill(color);
-        this.iconColor = color;
+        iconColorProperty().set(color);
     }
 
     @Nonnull
-    public Color getIconColor() {
-        return iconColor;
+    public Paint getIconColor() {
+        return iconColorProperty().get();
     }
 
     private void resolveSize(String description) {
@@ -116,14 +171,14 @@ public class FoundationIcon extends Text {
         }
     }
 
-    private void resolveColor(String description) {
+    private void resolvePaint(String description) {
         String[] parts = description.split(":");
         if (parts.length > 2) {
-            PropertyEditor editor = PropertyEditorResolver.findEditor(Color.class);
+            PropertyEditor editor = PropertyEditorResolver.findEditor(Paint.class);
             editor.setValue(parts[2]);
-            Color color = (Color) editor.getValue();
-            if (color != null) {
-                setIconColor(color);
+            Paint paint = (Paint) editor.getValue();
+            if (paint != null) {
+                setIconColor(paint);
             }
         }
     }
